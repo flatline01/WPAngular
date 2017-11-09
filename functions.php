965593c5272652b372b7691724f4d05bf5ee8fec
Joe Porto <cthulhu0@gmail.com>
2017-11-09 10:35:57 -0500

    Updates

--- functions.php
+++ functions.php
@@ -9,6 +9,9 @@ remove_action('wp_head', 'wlwmanifest_link');
 remove_action('wp_head', 'rsd_link');
 add_filter('emoji_svg_url', '__return_false');
 
+function my_deregister_scripts(){wp_deregister_script( 'wp-embed' );}
+add_action( 'wp_footer', 'my_deregister_scripts' );
+
 function remove_generator() {return '';}
 add_filter('the_generator', 'remove_generator');
 
@@ -17,12 +20,17 @@ function stb_dns_prefetch() {
 	echo '<meta http-equiv="x-dns-prefetch-control" content="on">
 	<link rel="dns-prefetch" href="//fonts.googleapis.com" />
 	<link rel="dns-prefetch" href="//fonts.gstatic.com" />';
+	
 }
 add_action('wp_head', 'stb_dns_prefetch', 0);
 
 //add our inits
-function header_init() {
-	wp_enqueue_style( get_template_directory_uri() . '/style.css' );
-	
+function wpangular_init() {
+	wp_enqueue_style( 'wpangular',get_stylesheet_directory_uri() . '/css/default.css', null, null, true);
+	wp_register_script( 'Angular', 'https://cdnjs.cloudflare.com/ajax/libs/angular.js/1.5.11/angular.min.js', null, null, true );
+	wp_register_script( 'App', get_stylesheet_directory_uri() . '/lib/app.js', null, null, true );
+	wp_enqueue_script('Angular');
+	wp_enqueue_script('App');
 }
-add_action( 'wp_enqueue_scripts', 'header_init', 20 );
+add_action( 'wp_enqueue_scripts', 'wpangular_init', 20 );
+
--- header.php
+++ header.php
@@ -1,9 +1,9 @@
 <!DOCTYPE html>
-<html <?php language_attributes(); ?> class="no-js">
+<html <?php language_attributes(); ?> class="no-js" >
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width">
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> ng-app>
